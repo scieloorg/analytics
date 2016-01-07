84b991bac6609af73a7f06ef2e31c79fa0e00c4e
fabiobatalha <fabiobatalha@gmail.com>
2016-01-07 18:23:07 -0200

    Gráfico de licença de uso por ano de publicação do documento

--- analytics/charts_config.py
+++ analytics/charts_config.py
@@ -283,6 +283,33 @@ class ChartsConfig(object):
 
         return {'options': chart}
 
+    def publication_article_licenses_by_publication_year(self, data):
+
+        # name = {'citable_documents': self._(u'Documentos citáveis'), 'not_citable_documents': self._(u'Documentos não citáveis')}
+
+        # for i, serie in enumerate(data['series']):
+        #     data['series'][i]['name'] = name[serie['name']]
+
+        chart = self.highchart
+        chart['chart']['type'] = 'line'
+        chart['title'] = {'text': self._(u'Distribuição de licenças de uso por ano de publicação')}
+        chart['xAxis'] = {
+            'categories': data['categories'],
+            'title': {'text': None}
+            }
+        chart['legend'] = {'enabled': True}
+        chart['series'] = data['series']
+        chart['yAxis']['title'] = {'text': self._(u'Número de documentos') }
+        chart['tooltip'] = {
+            'shared': True,
+            'useHTML': True,
+            'headerFormat': self._(u'Ano de publicação') + ' <strong>{point.key}</strong><table>',
+            'pointFormat': u'<tr><td><span style="color:{point.color}">\u25CF</span> {series.name}: </td><td style="text-align: right"><strong>{point.y}</strong></td><td style="text-align: right">&nbsp;({point.percentage:.2f}%)</td></tr>',
+            'footerFormat': '</table>'
+        }
+
+        return {'options': chart}
+
     def publication_article_licenses(self, data):
 
         chart = self.highchart
---
+++ analytics/templates/website/publication_article_licenses_publication_year.mako
@@ -0,0 +1,31 @@
+## coding: utf-8
+<div class="row container-fluid">
+  <div class="col-md-8">
+    <div id="article_licenses_publication_year" style="width100%; height:400px;">
+        <span id="loading_licenses_publication_year">
+            <img src="/static/images/loading.gif" />
+            <h5>${_(u'loading')}</h5>
+        </span>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">${_(u'Sobre o gráfico')}</h3>
+      </div>
+      <div class="panel-body">
+        ${_(u'Este gráfico apresenta a distribuição de documentos por licença de uso e ano de publicação. Os números são relacionados a coleção ou ao periódico quando um periódico é selecionado. A disponibilidade de indicadores de licença de uso dependem da adoção da coleção ou periódico selecionado de licenças de uso creative commons.')}
+      </div>
+    </div>
+  </div>
+</div>
+<script language="javascript">
+    $("#loading_licenses_publication_year").show();
+    $(document).ready(function() {
+        var url =  "${request.route_url('publication_article_licenses_publication_year')}?code=${selected_code}&collection=${selected_collection_code}&callback=?";
+        $.getJSON(url,  function(data) {
+            $('#article_licenses_publication_year').highcharts(data['options']);
+            $("#loading_licenses_publication_year").hide();
+        });
+    });
+</script>
