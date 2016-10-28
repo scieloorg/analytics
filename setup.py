92aca527e44053d7e1c7550923c4f1685c6f60a9
fabiobatalha <fabiobatalha@gmail.com>
2016-10-28 18:17:01 -0200

    Compatibilidade com ArticleMeta

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requires = []
 
 setup(
     name="analytics",
-    version='1.10.1',
+    version='1.11.1',
     description="A analytics frontend for SciELO usage and publication statistics",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
