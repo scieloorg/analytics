b28c857989d22146cc66261b790797ba183b9a8b
fabiobatalha <fabiobatalha@gmail.com>
2016-12-20 17:04:49 -0200

    Ajuste de versões

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ install_requires = [
     'pylibmc>=1.5.0',
     'scieloh5m5>=1.5.4',
     'xylose>=1.16.5',
-    'articlemetaapi>=1.6.13'
+    'articlemetaapi>=1.6.13',
+    'citedbyapi>=1.3.7'
     ]
 
 test_requires = []
