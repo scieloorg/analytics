92f09e11a9b4a3fb09cfd60a58570d63621af7e8
fabiobatalha <fabiobatalha@gmail.com>
2017-03-29 11:34:52 -0300

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
     'pylibmc>=1.5.0',
     'scieloh5m5>=1.5.4',
     'xylose>=1.16.5',
-    'articlemetaapi>=1.6.16',
+    'articlemetaapi>=1.8.16',
     'citedbyapi>=1.3.10'
     ]
 
